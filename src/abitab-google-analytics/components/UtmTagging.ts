e268b0824a92cac09782f74696b485bcaf019f16
OsvaldoCintra <osvaldo.cintra@abitab.com.uy>
2023-06-07 13:00:02 -0300

    Fixing utm

--- src/abitab-google-analytics/components/UtmTagging.ts
+++ src/abitab-google-analytics/components/UtmTagging.ts
@@ -1,11 +1,11 @@
-import ReactGA from "react-ga4";
 
-const UTM_SOURCE = "twitter";
-const UTM_MEDIUM = "paid-social";
-const UTM_CAMPAIGN = "blog-posts";
-const UTM_TERM = "ages_21-34";
-const UTM_CONTENT = "image-1";
-const UTM_TAGGING = `?${UTM_SOURCE}&${UTM_MEDIUM}&${UTM_CAMPAIGN}`;
+// const UTM_SOURCE = "twitter";
+// const UTM_MEDIUM = "paid-social";
+// const UTM_CAMPAIGN = "blog-posts";
+// const UTM_TERM = "ages_21-34";
+// const UTM_CONTENT = "image-1";
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// const UTM_TAGGING = `?${UTM_SOURCE}&${UTM_MEDIUM}&${UTM_CAMPAIGN}`;
 
 // export const utmTagging = (eventName: string, eventCategory: string) => {
 //   ReactGA.event({
